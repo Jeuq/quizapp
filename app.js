24bf1977854f76098f5fc8973fcd38eec323fe97
Jacq Babb <jacq@jeuq.me>
2020-10-20 11:42:16 -0700

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,95 @@
+/**
+ * Example store structure
+ */
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'What color is broccoli?',
+      answers: [
+        'red',
+        'orange',
+        'pink',
+        'green'
+      ],
+      correctAnswer: 'green'
+    },
+    {
+      question: 'What is the current year?',
+      answers: [
+        '1970',
+        '2015',
+        '2019',
+        '2005'
+      ],
+      correctAnswer: '2019'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
+
+/**
+ * 
+ * Technical requirements:
+ * 
+ * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * See your course material and access support for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ * 
+ */
+
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+
+// These functions return HTML templates
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+/********** EVENT HANDLER FUNCTIONS **********/
+
+// These functions handle events (submit, click, etc)
+
+
+function startPage(){
+// loads first 
+}
+
+
+function handleStartQuiz () {
+// click -> starting quiz
+}
+
+
+function render() {
+// depending on status of store.quizStarted
+// load startPage or
+// execute questionPage (.html)
+}
+
+
+function main() {
+  
+}
+
+
+function questionPage () {
+// reference store.questionnumber
+// template for formatting of question
+// template for radio button labels and values for options
+// submit button
+// increment store.questionnumber
+// send to answerPage
+}
+
+
+function answers() 
+// if correct send to correctPage
+// if incorrect send to incorrectPage
